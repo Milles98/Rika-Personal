894ca6bc90fe4732f06a76d80769ac6756d2234a
Nant1ng <andressantana99@hotmail.se>
2024-11-03 23:13:49 +0100

    react-router-dom

---
+++ Rika/src/views/Home.jsx
@@ -0,0 +1,11 @@
+import Title from "./sections/homepage/Title";
+
+const Home = () => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <Title />
+    </div>
+  );
+};
+
+export default Home;
---
+++ Rika/src/views/sections/homepage/Title.jsx
@@ -0,0 +1,12 @@
+const Title = () => {
+  return (
+    <>
+      <h1 className="text-2xl">Welcome to</h1>
+      <h1 className="text-6xl font-bold underline decoration-[#00d8ff]">
+        RIKA!
+      </h1>
+    </>
+  );
+};
+
+export default Title;

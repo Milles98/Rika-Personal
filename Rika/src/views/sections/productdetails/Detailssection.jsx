4cc54d0d994e96abd5104aae4cf2bf0bc349de91
JohnoAlm <almenoliver02@gmail.com>
2024-11-06 14:07:06 +0100

    Added color palette buttons

--- Rika/src/views/sections/productdetails/Detailssection.jsx
+++ Rika/src/views/sections/productdetails/Detailssection.jsx
@@ -24,11 +24,18 @@ const Detailssection = () => {
 
             <div className='flex-col gap-4 px-4 py-6'>
                 <h1 className='text-black font-mont text-[18px] font-extrabold leading-[150%]'>Size</h1>
-                <div className='flex'>
+                <div className='flex justify-center'>
                     <div className='flex gap-4 py-4'>
                         <button className='bg-white border border-2 hover:bg-black hover:text-white text-black font-bold py-2 px-4 rounded-full'>S</button>
                         <button className='bg-white border border-2 hover:bg-black hover:text-white text-black font-bold py-2 px-3 rounded-full'>M</button>
                         <button className='bg-white border border-2 hover:bg-black hover:text-white text-black font-bold py-2 px-4 rounded-full'>L</button>
+                    </div>
+                    <div className='grow'></div>
+                    <div className='flex gap-3 py-6 px-3 border rounded-full'>
+                        <button className='bg-[#d9ccce] border border-2 hover:bg-[#d9ccce]/[.5] hover:text-white text-black font-bold px-3 rounded-full'></button>
+                        <button className='bg-[#4d4a5d] border border-2 hover:bg-[#4d4a5d]/[.5] hover:text-white text-black font-bold px-3 rounded-full'></button>
+                        <button className='bg-[#222222] border border-2 hover:bg-[#222222]/[.5] hover:text-white text-black font-bold px-3 rounded-full'></button>
+                        <button className='bg-[#ffffff] border border-2 hover:bg-[#808080]/[.1] hover:text-white text-black font-bold px-3 rounded-full'></button>
                     </div>
                 </div>
             </div>

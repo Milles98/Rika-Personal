bb467dcc813bc2a8626626bc245e13c6264e1e35
kahol <kahola@gmail.com>
2024-11-06 13:58:16 +0100

    added auth provider to our routes.

--- Rika/src/main.jsx
+++ Rika/src/main.jsx
@@ -8,20 +8,25 @@ import "./assets/css/main.css";
 
 import Home from "./views/Home";
 import Products from "./views/Products";
-import Login from "./views/sections/loginpage/Login.jsx";
-import CustomerLoggedIn from "./views/sections/loginpage/CustomerLoggedIn.jsx";
+import Login from "./views/Login.jsx";
+import CustomerLandingPage from "./views/customerpages/CustomerLandingPage.jsx";
+import {AuthProvider} from "./lib/authorizeRole.jsx";
+import AdminLandingPage from "./views/adminpages/AdminLandingPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <FetchProductsProvider>
         <div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/CustomerLoggedIn" element={<CustomerLoggedIn />} />
-          </Routes>
+          <AuthProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/customer" element={<CustomerLandingPage />} />
+              <Route path="/admin" element={<AdminLandingPage />} />
+            </Routes>
+          </AuthProvider>
         </div>
       </FetchProductsProvider>
     </BrowserRouter>

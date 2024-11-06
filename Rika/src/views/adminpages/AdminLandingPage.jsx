7b00904212a6b2602c0b83275dcae5a56f90ae3b
Dessezar <Cristoffer.Dessezar@hotmail.com>
2024-11-06 14:58:33 +0100

    added create product button to admin page

--- Rika/src/views/adminpages/AdminLandingPage.jsx
+++ Rika/src/views/adminpages/AdminLandingPage.jsx
@@ -1,9 +1,16 @@
 ﻿import {useAuth} from "../../lib/authorizeRole.jsx";
 import {useCookies} from "react-cookie";
+import { useNavigate } from 'react-router-dom';
 
 const AdminLandingPage = () => {
     const {userRole, isAuthenticated} = useAuth();
     const [cookies] = useCookies(["jwt"]);
+    
+    const navigate = useNavigate();
+    
+    const toCreateProduct = () => {
+      navigate('/productscreate');
+    };
 
     if (!isAuthenticated) {
         return <div>I am not authenticated.</div>;
@@ -16,6 +23,13 @@ const AdminLandingPage = () => {
     return (
         <div>
             <h1>Welcome admin!</h1>
+            <button onClick={toCreateProduct}
+                    id="createButton"
+                    type="submit"
+                    className="w-[130px] bg-green-500 text-white p-2 rounded hover:bg-green-800 mt-4">
+                    Create Product
+                </button>
+
         </div>
     );
 };

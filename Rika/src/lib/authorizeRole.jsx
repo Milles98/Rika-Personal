953d46036059bd27688d82e0f54e7bf1b07d9732
kahol <kahola@gmail.com>
2024-11-06 13:57:21 +0100

    Added an authorization use context file. Checks JWT that we receive from our backend.

--- Rika/src/lib/authorizeRole.jsx
+++ Rika/src/lib/authorizeRole.jsx
@@ -1,57 +1,35 @@
-// src/context/AuthContext.jsx
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import {jwtDecode} from 'jwt-decode';
-import { useNavigate } from 'react-router-dom';
+import {useCookies} from "react-cookie";
 
-// Create an AuthContext
 const AuthContext = createContext();
 
-// Custom hook to access AuthContext
 export const useAuth = () => useContext(AuthContext);
 
-// AuthProvider component
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const navigate = useNavigate();
+    const [cookies] = useCookies(["jwt"]);
+    const [userRole, setUserRole] = useState("");
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    // Load user from the token if available
     useEffect(() => {
-        const token = localStorage.getItem("jwtToken");
-        if (token) {
+        const token = cookies.jwt;
+
+        if (token){
             try {
-                const decoded = jwtDecode(token);
-                
-                // Check token expiration
-                if (decoded.exp * 1000 > Date.now()) {
-                    setUser(decoded);
-                } else {
-                    // Token expired
-                    localStorage.removeItem("jwtToken");
-                }
+                const decodedToken = jwtDecode(token);
+                setUserRole(decodedToken.role);
+                setIsAuthenticated(true);
             } catch (error) {
-                console.error("Invalid token:", error);
+                console.log(error)
+                setIsAuthenticated(false);
             }
+        } else {
+            setIsAuthenticated(false);
         }
-    }, []);
-
-    const login = (token) => {
-        try {
-            const decoded = jwtDecode(token);
-            localStorage.setItem("jwtToken", token);
-            setUser(decoded);
-        } catch (error) {
-            console.error("Error decoding token:", error);
-        }
-    };
-
-    const logout = () => {
-        localStorage.removeItem("jwtToken");
-        setUser(null);
-        navigate('/login');
-    };
+    }, [cookies]);
 
     return (
-        <AuthContext.Provider value={{ user, login, logout }}>
+        <AuthContext.Provider value={{ userRole, isAuthenticated }}>
             {children}
         </AuthContext.Provider>
     );

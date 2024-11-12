364fd9cc3d88b1011cc0f8318d7c8d04c3c86696
Hossén Rahimzadegan <maulvids@gmail.com>
2024-11-12 10:18:19 +0100

    test commit

---
+++ Rika/src/views/ProductDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ImageSection from './sections/productdetails/ImageSection'
+import Navbar from './sections/productdetails/Navbar'
+import Detailssection from './sections/productdetails/Detailssection'
+const ProductDetails = () => {
+    return (
+        <>
+            <Navbar />
+            <ImageSection />
+            <Detailssection />
+        </>
+    )
+}
+
+export default ProductDetails
